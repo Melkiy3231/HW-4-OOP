60ff770a989918de3d9459c062a254f134520cb5
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:36:39 +0300

    Update Cuboid.java

--- src/main/java/com/oop/shapes/Cuboid.java
+++ src/main/java/com/oop/shapes/Cuboid.java
@@ -2,38 +2,49 @@ package main.java.com.oop.shapes;
 
 import main.java.com.oop.shapes.base.SpaceShape;
 
+import main.java.com.oop.vertex.Vertex;
 import main.java.com.oop.vertex.Vertex3D;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Cuboid extends SpaceShape {
-    private final double width;
-    private final double height;
-    private final double depth;
+    private Vertex3D vertex3D;
+    private double width;
+    private double height;
+    private double depth;
 
-    public Cuboid(Vertex3D vertex3D, String name, double width, double height, double depth) {
-        super(Arrays.asList(vertex3D), name);
+    public Cuboid(Vertex3D point3D, double width, double height, double depth) {
+        super(new ArrayList<>(Collections.emptyList()));
+        this.vertex3D = point3D;
         this.width = width;
         this.height = height;
         this.depth = depth;
     }
 
-    @Override
-    public String toString() {
-        return super.toString() +
-                "width=" + width +
-                ", height=" + height +
-                ", depth=" + depth +
-                '}';
+    public Cuboid(List<Vertex> points) {
+        super(points);
     }
 
     @Override
     public double getArea() {
-        return 2 * (width * height + width * depth + height * depth);
+        return 2 * (width * height + height * depth + depth * width);
     }
 
     @Override
     public double getVolume() {
         return width * height * depth;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuboid " +
+                " vertex3D=" + vertex3D +
+                " width=" + width +
+                " height=" + height +
+                " depth=" + depth +
+                " area=" + getArea() +
+                " volume=" + getVolume();
     }
 }
