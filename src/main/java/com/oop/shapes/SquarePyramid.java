3766c1398466bad566aafb6017d335de7c5ec831
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:37:40 +0300

    Update SquarePyramid.java

--- src/main/java/com/oop/shapes/SquarePyramid.java
+++ src/main/java/com/oop/shapes/SquarePyramid.java
@@ -3,37 +3,34 @@ package main.java.com.oop.shapes;
 import main.java.com.oop.shapes.base.SpaceShape;
 import main.java.com.oop.vertex.Vertex3D;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class SquarePyramid extends SpaceShape {
-    private final int baseWidth;
-    private final int height;
+    private double baseWidth;
+    private double pyramidHeight;
 
-    public SquarePyramid(Vertex3D vertex3D, int baseWidth, int height) {
-        super(List.of(vertex3D), "SquarePyramid");
+    public SquarePyramid(Vertex3D pointA, Vertex3D pointB, Vertex3D pointC, Vertex3D pointD, Vertex3D pointE, double baseWidth, double pyramidheight) {
+        super(Arrays.asList(pointA, pointB, pointC, pointD, pointE));
         this.baseWidth = baseWidth;
-        this.height = height;
+        this.pyramidHeight = pyramidheight;
     }
 
     @Override
     public double getArea() {
-        return Math.pow(baseWidth, 2)
-                + (2 * baseWidth
-                * Math.sqrt((Math.pow(baseWidth, 2) / 4)
-                + Math.pow(height, 2)));
+        return (4 * baseWidth) / 2 * ((baseWidth / (2 * Math.tan(Math.toRadians(45))))
+                + Math.sqrt(Math.pow(pyramidHeight, 2) + (Math.pow(baseWidth / 2 * Math.tan(Math.toRadians(45)), 2))));
     }
 
     @Override
     public double getVolume() {
-        return Math.pow(height, 2) * (baseWidth / 3.0);
+        return Math.pow(baseWidth, 2) * pyramidHeight / 3;
     }
 
     @Override
     public String toString() {
-        return "SquarePyramid: " + "\n" +
-                "baseWidth=" + baseWidth + "\n" +
-                "height=" + height + "\n" +
-                "Area : " + getArea() + "\n" +
-                "Volume: " + getVolume() + "\n";
+        return "SquarePyramid" +
+                getPoints() +
+                " baseWidth=" + baseWidth +
+                " pyramidHeight=" + pyramidHeight;
     }
 }
