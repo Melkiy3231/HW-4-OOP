effb48d500f52755cdf2808bd47ad86311ff7204
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:36:18 +0300

    Update Circle.java

--- src/main/java/com/oop/shapes/Circle.java
+++ src/main/java/com/oop/shapes/Circle.java
@@ -1,32 +1,37 @@
 package main.java.com.oop.shapes;
-import main.java.com.oop.interfaces.AreaMeasurable;
-import main.java.com.oop.interfaces.PerimeterMeasurable;
+
 import main.java.com.oop.shapes.base.PlaneShape;
 import main.java.com.oop.vertex.Vertex2D;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Circle extends PlaneShape  {
-    private final double radius;
+public class Circle extends PlaneShape {
+    private Vertex2D center;
+    private double radius;
 
-    public Circle(Vertex2D vertex, String name, double radius) {
-        super(Arrays.asList(vertex), name);
+    public Circle(Vertex2D center, double radius) {
+        super(new ArrayList<>(Collections.singletonList(center)));
+        this.center = center;
         this.radius = radius;
     }
 
     @Override
     public double getArea() {
-        return Math.PI * Math.pow(radius, 2);
+        return Math.PI * (this.radius * this.radius);
     }
 
     @Override
     public double getPerimeter() {
-        return 2 * Math.PI * radius;
+        return 2 * Math.PI * this.radius;
     }
 
     @Override
     public String toString() {
-        return super.toString() +
-                "radius=" + radius;
+        return "Circle " +
+                " point2D=" + center +
+                " radius=" + radius +
+                " perimeter=" + getPerimeter() +
+                " area=" + getArea();
     }
 }
