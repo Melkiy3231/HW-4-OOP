d7694ab0cd36c1d6f5c926f60ac9d10f23ffa82e
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:38:43 +0300

    Update Vertex2D.java

--- src/main/java/com/oop/vertex/Vertex2D.java
+++ src/main/java/com/oop/vertex/Vertex2D.java
@@ -1,23 +1,33 @@
 package main.java.com.oop.vertex;
-public class Vertex2D implements Vertex {
+
+public  class Vertex2D extends Vertex  {
     private final double x;
     private final double y;
 
+
     public Vertex2D(double x, double y) {
+        super(x, y);
         this.x = x;
         this.y = y;
     }
 
     public double getX() {
         return x;
+
     }
 
     public double getY() {
         return y;
+
     }
 
     @Override
     public String toString() {
         return "{x = " + x + ", y = " + y + "}";
+
+    }
+
+    public double getDistance(Vertex2D a, Vertex2D b) {
+        return 0;
     }
 }
