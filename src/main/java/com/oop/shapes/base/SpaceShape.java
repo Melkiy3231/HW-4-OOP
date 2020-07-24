5e919a9063b4a44c2fca41ba13eeed324f938bd3
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:35:58 +0300

    Update SpaceShape.java

--- src/main/java/com/oop/shapes/base/SpaceShape.java
+++ src/main/java/com/oop/shapes/base/SpaceShape.java
@@ -1,13 +1,22 @@
 package main.java.com.oop.shapes.base;
-import com.costaroot.metrics.AreaMeasurable;
-import com.costaroot.metrics.VolumeMeasurable;
-import com.costaroot.vertex.Vertex3D;
+
+import main.java.com.oop.interfaces.AreaMeasurable;
+import main.java.com.oop.interfaces.VolumeMeasurable;
+import main.java.com.oop.vertex.Vertex3D;
 import java.util.List;
 
-public class SpaceShape {
-    public abstract class SpaceShape extends Shape implements AreaMeasurable, VolumeMeasurable {
+public abstract class SpaceShape {
+    public SpaceShape(List<Vertex3D> asList, String name) {
 
-        public SpaceShape(List vertex3D, String name) {
+    }
+    public abstract double getArea();
+
+    public abstract double getVolume();
+
+    public abstract class spaceShape extends Shape implements AreaMeasurable, VolumeMeasurable {
+
+        public spaceShape(List vertex3D, String name){
+
             super(vertex3D, name);
         }
     }
