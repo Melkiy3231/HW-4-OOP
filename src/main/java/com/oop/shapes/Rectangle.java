a08a064a4b1d4ad06305cc147a4bfd1eb02a8e72
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:36:59 +0300

    Update Rectangle.java

--- src/main/java/com/oop/shapes/Rectangle.java
+++ src/main/java/com/oop/shapes/Rectangle.java
@@ -1,19 +1,33 @@
 package main.java.com.oop.shapes;
 
 import main.java.com.oop.shapes.base.PlaneShape;
+import main.java.com.oop.vertex.Vertex;
 import main.java.com.oop.vertex.Vertex2D;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Rectangle extends PlaneShape {
-
-    private final double width;
-    private final double height;
+    private Vertex2D vertex2D;
+    private double width;
+    private double height;
 
     public Rectangle(Vertex2D vertex2D, double width, double height) {
-        super(List.of(vertex2D), "Rectangle");
+        super(new ArrayList<>(Collections.singletonList(vertex2D)));
+        this.vertex2D = vertex2D;
         this.width = width;
         this.height = height;
+    }
+
+    @Override
+    public List<Vertex> getPoints() {
+        return super.getPoints();
+    }
+
+    @Override
+    public void setPoints(List<Vertex> points) {
+        super.setPoints(points);
     }
 
     @Override
@@ -23,15 +37,17 @@ public class Rectangle extends PlaneShape {
 
     @Override
     public double getPerimeter() {
-        return (width + height) * 2;
+        return 2 * (width + height);
     }
 
     @Override
     public String toString() {
-        return "Rectangle: " + "\n" +
-                "width = " + width + "\n"
-                + "height = " + height + "\n"
-                + "Area : " + getArea() + "\n"
-                + "Perimeter : " + getPerimeter() + "\n";
+        return "Rectangle" +
+                " point2D=" + vertex2D +
+                ", width=" + width +
+                ", height=" + height +
+                ", perimeter=" + getPerimeter() +
+                ", area=" + getArea() +
+                '}';
     }
 }
