a17748e0e207566609c92a949a1a71a07cd3ac3c
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:35:42 +0300

    Update Shape.java

--- src/main/java/com/oop/shapes/base/Shape.java
+++ src/main/java/com/oop/shapes/base/Shape.java
@@ -1,19 +1,30 @@
+
 package main.java.com.oop.shapes.base;
+
+import main.java.com.oop.vertex.Vertex;
+
 import java.util.List;
-public class Shape {
-        private final List vertex;
-        private final String name;
 
-        public Shape(List vertex, String name) {
-            this.vertex = vertex;
-            this.name = name;
-        }
+public abstract class Shape {
 
-        @Override
-        public String toString() {
-            return "Shape{" +
-                    "vertex=" + vertex +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
+    private List<Vertex> points;
+
+    public Shape(List<Vertex> points, String name) {
+        this.points = points;
     }
+
+    public List<Vertex> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Vertex> points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "points=" + points +
+                '}';
+    }
+}
