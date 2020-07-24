04f35c711d93c7f0ac8ba92c5d0812dbc558cd1d
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-24 19:38:26 +0300

    Update Vertex.java

--- src/main/java/com/oop/vertex/Vertex.java
+++ src/main/java/com/oop/vertex/Vertex.java
@@ -1,4 +1,35 @@
 package main.java.com.oop.vertex;
 
-public interface Vertex {
+public abstract class Vertex {
+    private double x;
+    private double y;
+
+
+    public Vertex(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double calculateDistance(Vertex point) {
+        return Math.sqrt(
+                Math.pow(this.x - point.getX(), 2)
+                        + Math.pow(this.y - point.getY(), 2));
+
+    }
 }
